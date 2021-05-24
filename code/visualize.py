47d88c92240952036a01f2503b400391d10dc6cd
pranshupant <pranshupant@gmail.com>
2021-05-24 00:58:06 -0400

    MSE_plot

--- code/visualize.py
+++ code/visualize.py
@@ -38,12 +38,62 @@ def plot_simulate(mse):
     plt.savefig(f"../{mode}/{dataset_name}/plots/mse_lineplot.png", bbox_inches='tight',  dpi=600)
     plt.close()
 
+def MSE_barplot():
+
+    mse_values = []
+    xticks = []
+
+    datasets= ['2d_cylinder', 'boussinesq', '2d_cylinder_CFD','SST','channel_flow']
+
+    for i in datasets:
+        try:
+            mse=np.load(f'../results/{i}/MSE.npy')
+            print(mse)
+            mse_values.append(-1*np.log(mse))
+            xticks.append(i)
+        except:
+            print(f'MSE for {i} not found')
+    
+    N = len(mse_values)            #number of the bars 
+    ind = np.arange(N)  # the x locations for the groups
+
+    fig = plt.figure(figsize = [4,3], dpi = 600)
+    width = 0.35
+    ax = plt.gca()
+    for axis in ['top','bottom','left','right']:
+        ax.spines[axis].set_linewidth(width)
+    tick_width = 0.35
+    plt.tick_params(direction = 'in', width = tick_width)
+
+    rects1 = ax.bar(ind, mse_values,width,color='blue',error_kw=dict(lw=1),capsize=2)
+    ww = 0.16
+    ax.set_ylabel('MSE per pixel', fontsize=10)
+    plt.xticks(ind,xticks,rotation=0, fontsize = 4)
+    plt.yticks(fontsize = 12)
+    plt.ylim((0,12))
+    plt.xlabel('DL-ROM', fontsize=10)
+    def autolabel(rects):
+        for rect in rects:
+            h = rect.get_height()
+            ax.text(rect.get_x()+rect.get_width()/2., 1.003*h, '%.2f'%float(h),
+                    ha='center', va='bottom',fontsize=8)
+    for axis in ['top','bottom','left','right']:
+        ax.spines[axis].set_linewidth(2)
+    plt.tick_params(direction = 'in', width = 1.5)
+
+    autolabel(rects1)
+
+    plt.show()
+    filename = '../results/MSE_barplot.png'
+    fig.savefig(filename, bbox_inches = 'tight')
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-d_set', dest='dset', type=str, default='2d_cylinder', help="Name of Dataset")
     parser.add_argument('-freq', dest='freq', type=int, default=20, help="Frequency for saving plots")
     parser.add_argument('-mode', dest='mode', type=str, default='results', help="result/simulate")
+    parser.add_argument('--MSE', dest='barplot', action='store_true')
 
     args = parser.parse_args()
     dataset_name = args.dset
@@ -72,3 +122,6 @@ if __name__ == '__main__':
         createAnimation(labels, dataset_name + "_ground_truth")
     else:
         print("Incorrect Mode!")
+
+    if args.barplot:
+        MSE_barplot()
